e44eae3051becfba3a545131c54c18eac0c26a62
davidvujic <david@vujic.se>
2016-09-21 15:21:36 +0200

    monkey patch session object

--- fake_requests.py
+++ fake_requests.py
@@ -1,42 +1,50 @@
 import requests
 
-original_get = requests.get
-original_post = requests.post
-original_delete = requests.delete
+_original_get = requests.get
+_original_post = requests.post
+_original_delete = requests.delete
+_original_session = requests.Session
+
+_responses = []
 
 
 def fake_request_maker():
-    def setup_fake_requests():
-        def make_fake_call(url, *args, **kwargs):
-            data, status_code = responses.pop(0)
-            return FakeHttpResponse(data, status_code)
-
-        requests.get = make_fake_call
-        requests.post = make_fake_call
-        requests.delete = make_fake_call
-
     def add(expected_response):
         if isinstance(expected_response, tuple):
             to_append = expected_response
         else:
             to_append = (expected_response, 200)
 
-        responses.append(to_append)
+        _responses.append(to_append)
 
-    responses = []
-    setup_fake_requests()
+    requests.get = _fake_call
+    requests.post = _fake_call
+    requests.delete = _fake_call
+    requests.Session = FakeSession
+    requests.session = FakeSession
 
     return add
 
 
 def reset():
-    requests.get = original_get
-    requests.post = original_post
-    requests.delete = original_delete
+    requests.get = _original_get
+    requests.post = _original_post
+    requests.delete = _original_delete
+    requests.Session = _original_session
+    requests.session = _original_session
+
+    global _responses
+    _responses = []
+
+
+class FakeSession:
+    def __init__(self):
+        self.get = _fake_call
+        self.post = _fake_call
+        self.delete = _fake_call
 
 
 class FakeHttpResponse:
-
     def __init__(self, message, status_code):
         self.text = message
         self.content = message
@@ -48,3 +56,8 @@ class FakeHttpResponse:
     def raise_for_status(self):
         if self.status_code >= 400:
             raise requests.HTTPError('Error!', response=self)
+
+
+def _fake_call(url, *args, **kwargs):
+    data, status_code = _responses.pop(0)
+    return FakeHttpResponse(data, status_code)
