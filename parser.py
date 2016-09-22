84e1bd033896cd4a892b6cacf8875024b7176432
davidvujic <david@vujic.se>
2016-09-22 20:50:07 +0200

    added a fake data file parser

---
+++ parser.py
@@ -0,0 +1,15 @@
+import json
+import os
+from xml.etree import ElementTree
+
+
+def with_fake_data_from(file_path, file_format='json'):
+    with open(file_path) as f:
+        if file_format == 'json':
+            expected = json.load(f)
+        elif file_format == 'xml':
+            expected = ElementTree.parse(f)
+        else:
+            expected = f.read()
+
+    return expected
